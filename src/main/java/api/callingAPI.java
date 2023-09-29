5ba52cce09ca69540f7a92c9d413332474e2de64
Razan Ahsan Rifandi <razan.rifandi@mail.utoronto.ca>
2023-09-29 18:49:08 -0400

    Changed callingAPI.java a little, continue later

--- src/main/java/api/callingAPI.java
+++ src/main/java/api/callingAPI.java
@@ -12,7 +12,7 @@ public class callingAPI {
     private static final String API_URL = "https://api.edamam.com/api/recipes/v2";
     private static final String APP_ID = "97884852";
     private static final String APP_KEY = "29ec3f53238d1c6ec3c16c6412bc91ea    —";
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JSONException {
         OkHttpClient client = new OkHttpClient().newBuilder().build();
         MediaType mediaType = MediaType.parse("application/json");
         JSONObject requestBody = new JSONObject();
@@ -31,9 +31,9 @@ public class callingAPI {
             Response response = client.newCall(request).execute();
             System.out.println(response);
             JSONObject responseBody = new JSONObject(response.body().string());
-
-            if (responseBody.getInt("status_code") == 200) {
-                System.out.println("The call succeed!");
+            System.out.println(responseBody);
+//            if (responseBody.getInt("status_code") == 200) {
+//                System.out.println("The call succeed!");
 //                JSONObject team = responseBody.getJSONObject("team");
 //                JSONArray membersArray = team.getJSONArray("members");
 //                String[] members = new String[membersArray.length()];
@@ -45,9 +45,9 @@ public class callingAPI {
 //                        .name(team.getString("name"))
 //                        .members(members)
 //                        .build();
-            } else {
-                throw new RuntimeException(responseBody.getString("message"));
-            }
+//            } else {
+//                throw new RuntimeException(responseBody.getString("message"));
+//            }
         }
         catch (IOException | JSONException e) {
             throw new RuntimeException(e);
