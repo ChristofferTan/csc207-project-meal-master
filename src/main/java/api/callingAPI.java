57124ece60fde1bc14bc02a4f15210ee624126f2
Janis Joplin <janisjoplin22404@gmail.com>
2023-09-29 22:38:31 -0400

    Finished callingAPI.java for one API calling and then print some information about the first recipe

--- src/main/java/api/callingAPI.java
+++ src/main/java/api/callingAPI.java
@@ -11,43 +11,39 @@ public class callingAPI {
     // Constants
     private static final String API_URL = "https://api.edamam.com/api/recipes/v2";
     private static final String APP_ID = "97884852";
-    private static final String APP_KEY = "29ec3f53238d1c6ec3c16c6412bc91ea    —";
+    private static final String APP_KEY = "29ec3f53238d1c6ec3c16c6412bc91ea";
     public static void main(String[] args) throws JSONException {
         OkHttpClient client = new OkHttpClient().newBuilder().build();
+        String type = "public";
+        String q = "chicken";
         MediaType mediaType = MediaType.parse("application/json");
-        JSONObject requestBody = new JSONObject();
-        requestBody.put("q", "chicken");
-        requestBody.put("app_id", APP_ID);
-        requestBody.put("app_key", APP_KEY);
-        RequestBody body = RequestBody.create(mediaType, requestBody.toString());
+        String requestUrl = API_URL + "?type=" + type + "&q=" + q + "&app_id=" + APP_ID + "&app_key=" + APP_KEY;
         Request request = new Request.Builder()
-                .url(API_URL)
-                .method("POST", body)
-//                .addHeader("Authorization", API_TOKEN)
+                .url(requestUrl)
+                .method("GET", null)
                 .addHeader("Content-Type", "application/json")
                 .build();
 
         try {
             Response response = client.newCall(request).execute();
-            System.out.println(response);
             JSONObject responseBody = new JSONObject(response.body().string());
-            System.out.println(responseBody);
-//            if (responseBody.getInt("status_code") == 200) {
-//                System.out.println("The call succeed!");
-//                JSONObject team = responseBody.getJSONObject("team");
-//                JSONArray membersArray = team.getJSONArray("members");
-//                String[] members = new String[membersArray.length()];
-//                for (int i = 0; i < membersArray.length(); i++) {
-//                    members[i] = membersArray.getString(i);
-//                }
-//
-//                return Team.builder()
-//                        .name(team.getString("name"))
-//                        .members(members)
-//                        .build();
-//            } else {
-//                throw new RuntimeException(responseBody.getString("message"));
-//            }
+            if (response.code() == 200) {
+                System.out.println("The call succeed! Some information about the first recipe is shown below.");
+                JSONObject recipe = responseBody
+                        .getJSONArray("hits")
+                            .getJSONObject(0)
+                                .getJSONObject("recipe");
+                System.out.println(recipe.getString("label"));
+                System.out.println("Calories: " + recipe.getFloat("calories"));
+                System.out.println("\nIngredients:");
+                JSONArray ingredients = recipe.getJSONArray("ingredientLines");
+                for (Object ingredient : ingredients) {
+                    System.out.println(ingredient);
+                }
+                System.out.println("\nInstructions at " + recipe.getString("url"));
+            } else {
+                throw new RuntimeException(responseBody.getString("message"));
+            }
         }
         catch (IOException | JSONException e) {
             throw new RuntimeException(e);
