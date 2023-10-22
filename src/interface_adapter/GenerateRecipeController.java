d3711bd6e3d07cde9558c59829a79f1ad7de0314
Christoffer Tan <tanchristoffer@gmail.com>
2023-10-22 18:14:04 -0400

    Generate Recipe Controller

---
+++ src/interface_adapter/GenerateRecipeController.java
@@ -0,0 +1,19 @@
+package interface_adapter;
+
+import use_case.GenerateRecipeInputBoundary;
+import use_case.GenerateRecipeInputData;
+
+public class GenerateRecipeController {
+    final GenerateRecipeInputBoundary generateRecipeInteractor;
+
+
+    public GenerateRecipeController(GenerateRecipeInputBoundary generateRecipeInteractor) {
+        this.generateRecipeInteractor = generateRecipeInteractor;
+    }
+    public void execute(String q, String[] diet, String[] health, String[] cuisineType, String[] mealType, String minCalories, String maxCalories) {
+        GenerateRecipeInputData generateRecipeInputData = new GenerateRecipeInputData(
+                q, diet, health, cuisineType, mealType, "calories=" + minCalories + "-" + maxCalories);
+
+        generateRecipeInteractor.execute(generateRecipeInputData);
+    }
+}
