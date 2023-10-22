793ab6f60fffa1d4d95d211b915bec18c94f18aa
Razan Ahsan Rifandi <razan.rifandi@mail.utoronto.ca>
2023-10-22 18:30:53 -0400

    Wrote GenerateRecipeOutputBoundary and GenerateRecipeOutputData

--- src/use_case/GenerateRecipeOutputBoundary.java
+++ src/use_case/GenerateRecipeOutputBoundary.java
@@ -1,4 +1,5 @@
 package use_case;
 
 public interface GenerateRecipeOutputBoundary {
+    void prepareSuccessView(GenerateRecipeOutputData recipe);
 }
--- src/use_case/GenerateRecipeOutputData.java
+++ src/use_case/GenerateRecipeOutputData.java
@@ -1,4 +1,31 @@
 package use_case;
 
 public class GenerateRecipeOutputData {
+    private final String label;
+    private final int calories;
+    private final String[] ingredients;
+    private final String recipeUrl;
+
+    public GenerateRecipeOutputData(String label, int calories, String[] ingredients, String recipeUrl) {
+        this.label = label;
+        this.calories = calories;
+        this.ingredients = ingredients;
+        this.recipeUrl = recipeUrl;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public String[] getIngredients() {
+        return ingredients;
+    }
+
+    public String getRecipeUrl() {
+        return recipeUrl;
+    }
 }
