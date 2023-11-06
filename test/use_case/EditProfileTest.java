0a069534787b423cd90502dcae8c7807f874fd3a
Razan Ahsan Rifandi <razan.ahsanrifandi@gmail.com>
2023-11-06 15:17:33 -0500

    Created edit profile test file (to be implemented later)

---
+++ test/use_case/EditProfileTest.java
@@ -0,0 +1,8 @@
+package use_case;
+
+public class EditProfileTest {
+    @org.junit.Test
+    public void testEditProfile() {
+        // TODO: create a junit test to make sure edit profile works as intended
+    }
+}
