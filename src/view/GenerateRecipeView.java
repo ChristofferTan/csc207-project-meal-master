accaf7fceec375fd8537c085c8254a363df8e942
faraazzz31 <faraaz.ahmed31@gmail.com>
2023-11-08 03:14:10 -0500

    Started Implementing GenerateRecipeView.java

--- src/view/GenerateRecipeView.java
+++ src/view/GenerateRecipeView.java
@@ -5,8 +5,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import interface_adapters.generate_recipe.GenerateRecipeViewModel;
 
 public class GenerateRecipeView extends JPanel implements ActionListener, PropertyChangeListener {
+
+    public final String viewName = "generate recipe";
+
+
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
