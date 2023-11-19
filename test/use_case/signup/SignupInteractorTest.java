b1b46cf2a3220a75f55777bdf1ed13c10e3d68b2
Christoffer Tan <tanchristoffer@gmail.com>
2023-11-19 14:32:11 -0500

    revised the test for sign up use case interactor

--- test/use_case/signup/SignupInteractorTest.java
+++ test/use_case/signup/SignupInteractorTest.java
@@ -2,6 +2,8 @@ package use_case.signup;
 
 import data_access.InMemoryUserDataAccessObject;
 import entity.CommonUserFactory;
+import entity.User;
+import entity.UserFactory;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.*;
@@ -9,7 +11,7 @@ import static org.junit.Assert.*;
 public class SignupInteractorTest {
     @Test
     void successTest() {
-        SignupInputData inputData = new SignupInputData("Faraaz", "Ahmed", "Ahmed");
+        SignupInputData inputData = new SignupInputData("Faraaz", "Ahmed", "Ahmed", "Faraaz", 19, "Man", 168, 60, "medium");
         SignupUserDataAccessInterface userRepository = new InMemoryUserDataAccessObject();
 
         // This creates a successPresenter that tests whether the test case is as we expect.
@@ -33,7 +35,7 @@ public class SignupInteractorTest {
 
     @Test
     void failurePasswordMismatchTest() {
-        SignupInputData inputData = new SignupInputData("Faraaz", "Ahmed", "wrong");
+        SignupInputData inputData = new SignupInputData("Faraaz", "Ahmed", "wrong", "Faraaz", 19, "Man", 168, 60, "medium");
         SignupUserDataAccessInterface userRepository = new InMemoryUserDataAccessObject();
 
         // This creates a presenter that tests whether the test case is as we expect.
@@ -56,13 +58,13 @@ public class SignupInteractorTest {
 
     @Test
     void failureUserExistsTest() {
-        SignupInputData inputData = new SignupInputData("Faraaz", "password", "wrong");
+        UserFactory factory = new CommonUserFactory();
+        User user1 = factory.create("Faraaz", "ganteng", "christoffer", 18, "Man", 170, 68, "medium");
+        SignupInputData inputData = new SignupInputData("Faraaz", "Ahmed", "Ahmed", "Faraaz", 19, "Man", 168, 60, "medium");
+
         SignupUserDataAccessInterface userRepository = new InMemoryUserDataAccessObject();
+        userRepository.save(user1);
 
-        // Add Paul to the repo so that when we check later they already exist
-
-
-        // This creates a presenter that tests whether the test case is as we expect.
         SignupOutputBoundary failurePresenter = new SignupOutputBoundary() {
             @Override
             public void prepareSuccessView(SignupOutputData user) {
