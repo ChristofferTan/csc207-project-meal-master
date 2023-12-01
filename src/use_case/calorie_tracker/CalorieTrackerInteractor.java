b4bf606085142a2515cb927bcda27c958d89e287
faraazzz31 <faraaz.ahmed31@gmail.com>
2023-12-01 14:39:53 -0500

    Modified the CalorieTrackerInteractor.java and CalorieTrackerOutputData.java to maintain CA.

--- src/use_case/calorie_tracker/CalorieTrackerInteractor.java
+++ src/use_case/calorie_tracker/CalorieTrackerInteractor.java
@@ -1,5 +1,11 @@
 package use_case.calorie_tracker;
 
+import entity.MealType;
+import entity.Planner;
+import entity.Recipe;
+
+import java.util.HashMap;
+
 public class CalorieTrackerInteractor implements CalorieTrackerInputBoundary{
     final CalorieTrackerDataAccessInterface dataAccessInterface;
     final CalorieTrackerOutputBoundary calorieTrackerPresenter;
@@ -13,10 +19,33 @@ public class CalorieTrackerInteractor implements CalorieTrackerInputBoundary{
     @Override
     public void execute(CalorieTrackerInputData calorieTrackerInputData) {
         try {
-            CalorieTrackerOutputData calorieTrackerOutputData = new CalorieTrackerOutputData(calorieTrackerInputData.getPlanner());
-            calorieTrackerPresenter.prepareSuccessView(calorieTrackerOutputData);
+            Planner planner = calorieTrackerInputData.getPlanner();
+            int weeklyCalories = calculateWeeklyCalories(planner);
+            int averageDailyCalories = calculateAverageDailyCalories(planner);
+
+            CalorieTrackerOutputData outputData = new CalorieTrackerOutputData(planner, weeklyCalories, averageDailyCalories);
         } catch (Exception e) {
             calorieTrackerPresenter.prepareFailView(e.getMessage());
         }
+    }
+
+    private int calculateWeeklyCalories(Planner planner) {
+        float calories = 0;
+        for (HashMap<MealType, Recipe> recipes: planner.getWeeklyRecipes().values()) {
+            for (Recipe recipe: recipes.values()) {
+                calories += recipe.getCalories();
+            }
+        }
+        return Math.round(calories);
+    }
+
+    private int calculateAverageDailyCalories(Planner planner) {
+        float calories = 0;
+        for (HashMap<MealType, Recipe> recipes: planner.getWeeklyRecipes().values()) {
+            for (Recipe recipe: recipes.values()) {
+                calories += recipe.getCalories();
+            }
+        }
+        return Math.round(calories / 7);
     }
 }
